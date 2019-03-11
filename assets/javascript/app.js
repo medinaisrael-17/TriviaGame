a9cafd0e5e78a645e81ca48bf9c9dba3e13f61ac
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-11 18:09:13 -0500

    Starting HTML and pseudo code for javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+//on button click 
+    //display the qustions 
+    //start the timer
+
+//while timer running
+    //use mouse to select answers 
+
+    //if user answer === correct
+        //add to correct by one
+
+    //else if user asnwer === incorrect 
+        //add to the incorrect by one 
+
+    //else 
+        //unaswered ++
+    
+//when time is over display correct, incorrect, unaswered
+
+
+
+    
