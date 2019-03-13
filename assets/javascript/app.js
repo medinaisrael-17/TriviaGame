675a86dabc3365fe7b603a842af09c04d28d5857
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-12 23:07:18 -0500

    added the final componets for the javascript as well as some html edits and changes to the css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,64 @@
-//on button click 
-    //display the qustions 
-    //start the timer
-
-//while timer running
-    //use mouse to select answers 
-
-    //if user answer === correct
-        //add to correct by one
-
-    //else if user asnwer === incorrect 
-        //add to the incorrect by one 
-
-    //else 
-        //unaswered ++
-    
-//when time is over display correct, incorrect, unaswered
+$(document).ready(function () {
 
 
+    //Declare our variables 
+    var correct = 0;
+    var incorrect = 0;
+    var time = 120;
+    var intervalId;
 
-    
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1200);
+    }
+
+    function decrement() {
+        time--;
+        $("#timer").text(time);
+        //when time is over display correct, incorrect, unaswered
+        if (time === 0) {
+            $("form").css("display", "none");
+            $("#gameOver").css("display", "block");
+            $("#correctCounter").text(correct);
+            $("#incorrectCounter").text(incorrect);
+        }
+    }
+    //on button click 
+    $("#start").on("click", function () {
+        //display the qustions 
+        $(this).css("display", "none");
+        $("form").css("display", "block");
+        //start the timer
+        run();
+
+    })
+
+    $("#done").on("click", function (event) {
+        event.preventDefault();
+        $("form").css("display", "none");
+        $("#gameOver").css("display", "block");
+        $("#correctCounter").text(correct);
+        $("#incorrectCounter").text(incorrect);
+        
+        
+    })
+
+    $("input").on("click", function(){
+        var inputValue = $(this);
+        if (inputValue.val() === "incorrect"){
+            incorrect++;
+            $(this).parents("#question").nextAll();
+        }
+        else {
+            correct++;
+            $(this).parents("#question").nextAll();''            
+        }
+       
+        
+    })
+
+
+
+
+
+})
